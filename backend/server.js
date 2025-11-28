9c09a055348b9f73d0c0bd9892481d7ab2c11bbb
lakshmi <lakshmipriyan0606@gmail.com>
2025-11-28 12:08:30 +0530

    fix express 5 wildcard routing

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
 connectDB();
 
-app.listen(5000, () => console.log("🚀 Server running on 5000"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
